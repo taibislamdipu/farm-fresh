786da628ef798392fe6f6628ce0e00d5f9ba0281
taibislamdipu <mailtaibislam@gmail.com>
2025-09-04 00:16:58 +0600

    new

--- components/header/Search.jsx
+++ components/header/Search.jsx
@@ -1,12 +1,15 @@
+import { IoSearch } from "react-icons/io5";
+
 export default function SearchProduct() {
   return (
-    <div className="hidden sm:block relative">
+    <div className="relative hidden sm:block">
       <input
         type="text"
         placeholder="Search products..."
-        className="w-64 pl-10 pr-4 py-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-primary-500 focus:border-transparent dark:bg-gray-700 dark:text-white"
+        className="w-64 rounded-lg border border-gray-300 py-2 pl-10 pr-4 focus:border-transparent focus:ring-2 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white"
       />
-      <i className="fas fa-search absolute left-3 top-3 text-gray-400"></i>
+
+      <IoSearch className="absolute left-3 top-3 text-gray-400" />
     </div>
   );
 }
