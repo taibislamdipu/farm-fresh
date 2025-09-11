896ed20798bb01ff199550ecd642b115920dd001
taibislamdipu <mailtaibislam@gmail.com>
2025-09-11 14:43:10 +0600

    seo done home page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
 import HomePage from "@/components/home/Home";
 
+export const metadata = {
+  title: "FarmFresh | Home",
+  description:
+    "Connect directly with local farmers and get the freshest produce delivered to your doorstep",
+};
+
 export default function Home() {
   return (
     <div>
